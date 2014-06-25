dda3fba5d7d05586020b241617fab4fbc244beee
daichimeller <s1190026@u-aizu.ac.jp>
2014-06-25 20:06:58 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtTown::Application.config.secret_key_base = 'c4ab890b377341783248054e1c54371fc89499525c1417b6e559c2d768ea542b0f3be85ecc4d32fc0281e3d6e2a1c3aba0579e65ea9ad990ddf5aa7d988ae5c4'
